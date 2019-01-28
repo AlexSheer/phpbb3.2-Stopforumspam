fd0eb7f8df869b1e08367e1067b52838e79c6848
AlexSheer <alex_sheer@inbox.lv>
2019-01-29 00:20:06 +0200

    Update listener.php

--- event/listener.php
+++ event/listener.php
@@ -16,6 +16,18 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 */
 class listener implements EventSubscriberInterface
 {
+	/** @var \phpbb\controller\helper */
+	protected $helper;
+
+	/** @var \phpbb	emplate	emplate */
+	protected $template;
+
+	/** @var \phpbb\auth\auth */
+	protected $auth;
+
+	/* @var \phpbb\db\driver\driver */
+	protected $db;
+
 /**
 * Assign functions defined in this class to event listeners in the core
 *
@@ -26,15 +38,74 @@ class listener implements EventSubscriberInterface
 	static public function getSubscribedEvents()
 	{
 		return array(
+			'core.user_setup'					=> 'load_language_on_setup',
+			'core.memberlist_view_profile'		=> 'chk_profile'
 		);
 	}
 
 	/**
 	* Constructor
 	*/
-	public function __construct(\phpbb\template\template $template, $phpbb_root_path)
+	public function __construct(
+		\phpbb\controller\helper $helper,
+		\phpbb\template\template $template,
+		\phpbb\auth\auth $auth,
+		\phpbb\db\driver\driver_interface $db
+	)
 	{
-		$this->template = $template;
-		$this->phpbb_root_path = $phpbb_root_path;
+		$this->helper				= $helper;
+		$this->template				= $template;
+		$this->auth					= $auth;
+		$this->db					= $db;
+	}
+
+	public function load_language_on_setup($event)
+	{
+		$lang_set_ext = $event['lang_set_ext'];
+		$lang_set_ext[] = array(
+			'ext_name' => 'sheer/stopforumspam',
+			'lang_set' => 'stopforumspam',
+		);
+		$event['lang_set_ext'] = $lang_set_ext;
+	}
+
+	public function chk_profile($event)
+	{
+		if ($this->auth->acl_get('a_') || $this->auth->acl_get('m_'))
+		{
+			$s_chk_sfs_allowed = true;
+			$member = $event['member'];
+			$sql = 'SELECT group_id
+				FROM ' . USER_GROUP_TABLE . '
+					WHERE user_id = ' . $member['user_id'];
+
+			$result = $this->db->sql_query($sql);
+			while ($row = $this->db->sql_fetchrow($result))
+			{
+				$groups[] = $row['group_id'];
+			}
+			$this->db->sql_freeresult($result);
+
+			foreach ($groups as $key => $group)
+			{
+				$sql = 'SELECT group_name
+					FROM ' . GROUPS_TABLE . '
+						WHERE group_id = ' . $group;
+				$result = $this->db->sql_query($sql);
+				$group_name = (string) $this->db->sql_fetchfield('group_name');
+				$this->db->sql_freeresult($result);
+
+				if ($group_name == 'GLOBAL_MODERATORS' || $group_name == 'ADMINISTRATORS')
+				{
+					$s_chk_sfs_allowed = false;
+					break;
+				}
+			}
+
+			$this->template->assign_vars(array(
+				'U_CHK_SFS'			=> $this->helper->route('sheer_stopforumspam_sfsfinder', array('u' => $member['user_id'])),
+				'S_CHK_SFS_ALLOWED'	=> $s_chk_sfs_allowed,
+			));
+		}
 	}
 }
